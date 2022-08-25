0f864c44045d4a7475f8c0534d371ac035dfe9b2
nhuthai.n <nhuthai.n@trustana.com>
2022-08-25 17:20:57 +0700

    add_alluew

--- cypress/e2e/TS01_Demo_Cypress/Google.cy.js
+++ cypress/e2e/TS01_Demo_Cypress/Google.cy.js
@@ -2,16 +2,30 @@ describe('google',()=>{
     beforeEach(() =>{
     })
 
-    before(function() {
+    before(()=> {
     })
-
-    it('DEMO_01 Search on Google',() => {
+    it('DEMO_01 Search success on Google',() => {
         cy.visit('https://www.google.com.vn/?hl=vi')
-        cy.log('111111111111111111111111111111111111')
         cy.xpath("//input[@class='gLFyf gsfi']")
             .type('minh')
             .type('{enter}')
         cy.url().should('contains','https://www.google.com.vn/search?q=minh&')
+    })
 
+    it('DEMO_01.1 Search success on Google',() => {
+        cy.visit('https://www.google.com.vn/?hl=vi')
+        cy.xpath("//input[@class='gLFyf gsfi']")
+            .type('minh')
+            .type('{enter}')
+        cy.url().should('contains','https://www.google.com.vn/search?q=minh&')
+    })
+
+    it('DEMO_02 Search fail on Google',() => {
+        cy.visit('https://www.google.com.vn/?hl=vi')
+        cy.xpath("//input[@class='gLFyf gsfi']")
+            .type('minh')
+            .type('{enter}')
+        cy.url().should('contains','https://www.google.com.vn/search?q=minh&')
+        cy.xpath("//input[@class='gLFyf gsfiasdasdasdassa']")
     })
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,7 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+import '@shelex/cypress-allure-plugin';
+// require('./commands')
 
 require('cypress-xpath')
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+require('@shelex/cypress-allure-plugin');
