0cbe12688d70528f34e93caba0e9d5b3df68d7be
minhvl1 <minhvu890yahoo@gmail.com>
2022-12-19 15:34:57 +0700

    add json file

--- cypress/e2e/TS03_DemoQA/practiceForm.cy.js
+++ cypress/e2e/TS03_DemoQA/practiceForm.cy.js
@@ -1,31 +1,32 @@
 describe("Practice Form",{defaultCommandTimeout:15000},()=>{
-
+    const inputValue = require("../../fixtures/inputValue.json")
     beforeEach("",()=>{
         cy.viewport(1920,1080)
         cy.visit("https://demoqa.com",{failOnStatusCode:false})
     })
 
     it("Submit practice form",()=>{
+
         cy.xpath("//div[@class='card mt-4 top-card'][2]").click()
         cy.xpath("//div[@class='element-list collapse show']").click()
 
         cy.xpath("//input[@id='firstName']")
             .clear()
             .should("be.empty")
-            .type("minh")
+            .type(inputValue.firstName)
         cy.xpath("//input[@id='lastName']")
             .clear()
             .should("be.empty")
-            .type("vu")
+            .type(inputValue.lastName)
         cy.xpath("//label[@for='gender-radio-3']").click()
 
         cy.xpath("//input[@id='userNumber']")
             .clear()
             .should("be.empty")
-            .type("1231231231")
+            .type(inputValue.phoneNumber)
 
-        cy.xpath("//div[@class=' css-yk16xz-control']").type("NCR{enter}")
-        cy.xpath("(//div[@class=' css-1hwfws3'])[2]").type("Delhi{enter}")
+        cy.xpath("//div[@class=' css-yk16xz-control']").type(inputValue.state+"{enter}")
+        cy.xpath("(//div[@class=' css-1hwfws3'])[2]").type(inputValue.city+"{enter}")
 
         cy.xpath("//button[@id='submit']").click()
 
@@ -35,19 +36,19 @@ describe("Practice Form",{defaultCommandTimeout:15000},()=>{
         cy.xpath("//td[text()='Student Name']/following-sibling::td")
             .invoke('text')
             .then((text) => {
-                assert.equal(text,"minh vu","equal input name")
+                assert.equal(text,inputValue.firstName+" "+inputValue.lastName,"equal input name")
       })
 
         cy.xpath("//td[text()='Mobile']/following-sibling::td")
             .invoke('text')
             .then((text) => {
-                assert.equal(text,"1231231231","equal number")
+                assert.equal(text,inputValue.phoneNumber,"equal number")
             })
 
         cy.xpath("//td[text()='State and City']/following-sibling::td")
             .invoke('text')
             .then((text) => {
-                assert.equal(text,"NCR Delhi","equal country")
+                assert.equal(text,inputValue.state+" "+inputValue.city,"equal country")
             })
     })
 
