8bb6ab307ccdeb4acb6e4353fbee08597f0b5f44
minhvl1 <minhvu890yahoo@gmail.com>
2022-12-20 11:12:25 +0700

    update command

--- cypress/e2e/TS03_DemoQA/practiceForm.cy.js
+++ cypress/e2e/TS03_DemoQA/practiceForm.cy.js
@@ -6,50 +6,12 @@ describe("Practice Form",{defaultCommandTimeout:15000},()=>{
     const inputValue = require("../../fixtures/inputValue.json")
     beforeEach("",()=>{
         cy.viewport(1920,1080)
-        cy.visit("https://demoqa.com",{failOnStatusCode:false})
+        cy.visit(Cypress.env("qaBaseUrl"),{failOnStatusCode:false})
     })
 
     it("Submit practice form",()=>{
-
-        homePageQa.goToPracticePage()
-        practiceForm.getPracticeForm()
-        practiceForm.inputFirstName()
-        practiceForm.inputLastName()
-        practiceForm.selectGender()
-        practiceForm.inputUserNumber()
-
-        practiceForm.clickDateOfBirthTextbox()
-        practiceForm.selectYearOfBirth()
-        practiceForm.selectMonthOfBirth()
-        practiceForm.selectDayOfBirth()
-
-        practiceForm.selectState()
-        practiceForm.selectCity()
-
-        practiceForm.clickSubmitButton()
-
-
-
-        cy.xpath("//div[contains(text(),'Thanks for submitting the form')]")
-            .should("be.visible")
-
-        cy.xpath("//td[text()='Student Name']/following-sibling::td")
-            .invoke('text')
-            .then((text) => {
-                assert.equal(text,inputValue.firstName+" "+inputValue.lastName,"equal input name")
-      })
-
-        cy.xpath("//td[text()='Mobile']/following-sibling::td")
-            .invoke('text')
-            .then((text) => {
-                assert.equal(text,inputValue.phoneNumber,"equal number")
-            })
-
-        cy.xpath("//td[text()='State and City']/following-sibling::td")
-            .invoke('text')
-            .then((text) => {
-                assert.equal(text,inputValue.state+" "+inputValue.city,"equal country")
-            })
+        cy.FillAndSubmitPracticeForm()
+        cy.VerifyPracticeForm()
     })
 
-    })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,9 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
+import HomePageQa from "../support/pageObject/demoQa/HomePageQa";
+import PracticeForm from "../support/pageObject/demoQa/PracticeForm";
+import inputValue from "../fixtures/inputValue.json";
+
+const homePageQa = new HomePageQa()
+const practiceForm = new PracticeForm()
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
@@ -23,3 +24,55 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+/**
+ * @memberOf cy
+ * @method FillAndSubmitPracticeForm
+ */
+
+Cypress.Commands.add("FillAndSubmitPracticeForm",()=>{
+    homePageQa.goToPracticePage()
+    practiceForm.getPracticeForm()
+    practiceForm.inputFirstName()
+    practiceForm.inputLastName()
+    practiceForm.selectGender()
+    practiceForm.inputUserNumber()
+
+    practiceForm.clickDateOfBirthTextbox()
+    practiceForm.selectYearOfBirth()
+    practiceForm.selectMonthOfBirth()
+    practiceForm.selectDayOfBirth()
+
+    practiceForm.selectState()
+    practiceForm.selectCity()
+
+    practiceForm.clickSubmitButton()
+
+})
+
+/**
+ * @memberOf cy
+ * @method VerifyPracticeForm
+ */
+
+Cypress.Commands.add("VerifyPracticeForm",()=>{
+    cy.xpath("//div[contains(text(),'Thanks for submitting the form')]")
+        .should("be.visible")
+
+    cy.xpath("//td[text()='Student Name']/following-sibling::td")
+        .invoke('text')
+        .then((text) => {
+            assert.equal(text,inputValue.firstName+" "+inputValue.lastName,"equal input name")
+        })
+
+    cy.xpath("//td[text()='Mobile']/following-sibling::td")
+        .invoke('text')
+        .then((text) => {
+            assert.equal(text,inputValue.phoneNumber,"equal number")
+        })
+
+    cy.xpath("//td[text()='State and City']/following-sibling::td")
+        .invoke('text')
+        .then((text) => {
+            assert.equal(text,inputValue.state+" "+inputValue.city,"equal country")
+        })
+})
